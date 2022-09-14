d99d75915f5fb8bf9bd8d7e9c9e743a4aed105a3
Blessing Bolaji <smarteematics@gmail.com>
2022-09-13 19:12:52 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,15 +4,17 @@
  * main - prints _putchar exactly
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int i;
-	char p[i] = "_putchar";
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(p[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
